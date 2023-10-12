6cfdecc455cc06c6f3e6aa8b54dd4b587006fb66
Timur <slepospb@gmail.com>
2023-10-13 00:32:48 +0300

    hotfix data models

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,63 +1,20 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Types } from "mongoose";
 
-export interface IProduct {
-  serialNumber: number;
-  // isNew: boolean;
-  photo: string;
-  title: string;
-  type: string;
-  specification: string;
-  guarantee: {
-    start: string;
-    end: string;
-  };
-  price: [
-    { value: number; symbol: string; isDefault: boolean },
-    { value: number; symbol: string; isDefault: boolean }
-  ];
-  order: number;
-  date: string;
-}
+
 
 export interface IOrder extends Document {
   title: string;
-  date: Date;
+  date: string;
   description: string;
-  products: IProduct[];
+  products: Types.ObjectId[];
 }
 
-const ProductSchema = new Schema<IProduct>({
-  serialNumber: { type: Number },
-  //isNew: { type: Boolean },
-  photo: { type: String },
-  title: { type: String },
-  type: { type: String },
-  specification: { type: String },
-  guarantee: {
-    start: { type: String },
-    end: { type: String },
-  },
-  price: [
-    {
-      value: { type: Number },
-      symbol: { type: String },
-      isDefault: { type: Boolean },
-    },
-    {
-      value: { type: Number },
-      symbol: { type: String },
-      isDefault: { type: Boolean },
-    },
-  ],
-  order: { type: Number },
-  date: { type: String },
-});
 
 const OrderSchema = new Schema<IOrder>({
   title: { type: String },
-  date: { type: Date },
+  date: { type: String },
   description: { type: String },
-  products: [ProductSchema],
+  products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
 });
 
 const OrderModel = mongoose.model<IOrder>("Order", OrderSchema);
